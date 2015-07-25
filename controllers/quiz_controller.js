d0d970324a7ce2b39cedb0b9692ba25b4b79d405
estela <estela@server.(none)>
2015-07-26 00:23:24 +0200

    PreparadoParaElPush

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,18 +1,17 @@
   var models = require('../models/models.js');
 
   // Autoload - factoriza el codigo si ruta incluye :quizId  
-  exports.load = function(req, res, next, quizId) {	 
+  exports.load = function(req, res, next, quizId) {	  //console.log("Estoy en LOAD");
 	  models.Quiz.find(quizId).then(
       function(quiz) { 
 	    if (quiz) { req.quiz = quiz; next(); } 
  		else { next( new Error('No existe quizId=' + quizId)) }
 					}              
-			  ).catch(function(error){ next(error)});
-			  console.log("Estoy en LOAD");
+			  ).catch(function(error){ next(error)});			  
 	};
 
     // GET /quizes
-  exports.index = function(req,res) {	console.log("Estoy en INDEX");
+  exports.index = function(req,res) {	//console.log("Estoy en INDEX");
 	models.Quiz.findAll().then(function(quizes) { 
 	  res.render('quizes/index.ejs', { quizes: quizes, errors: [] 
 	  }); }).catch(function(error) { next(error)});	
@@ -20,14 +19,14 @@
 
 
 	// GET /quizes/:id	
-  exports.show = function(req,res) {	console.log("Estoy en SHOW");
+  exports.show = function(req,res) {	//console.log("Estoy en SHOW");
     //models.Quiz.find(req.params.quizId).then(function(quiz) { 
       res.render('quizes/show', { quiz: req.quiz, errores: [] 
       });	
   };
 
 	// GET /quizes/:id/answer	
-  exports.answer = function(req,res) {	console.log("Estoy en ANSWER");
+  exports.answer = function(req,res) {	//console.log("Estoy en ANSWER");
 	var resultado = 'Incorrecto';
 	if ( req.query.respuesta === req.quiz.respuesta )
 	  { resultado = 'Correcto'; }
@@ -37,18 +36,18 @@
   };
   
 // GET /quizes/new
-  exports.new = function(req, res) {	console.log("Estoy en NEW");
+  exports.new = function(req, res) {	//console.log("Estoy en NEW");
 	var quiz = models.Quiz.build( // crea objeto quiz 
 	{ pregunta: "Pregunta", respuesta: "Respuesta" });
   res.render('quizes/new', { quiz: quiz, errores: []});
   };
 		// POST /quizes/create
-  exports.create = function(req, res){   console.log("Estoy en CREATE");
+  exports.create = function(req, res){   //console.log("Estoy en CREATE");
     var quiz = models.Quiz.build( req.body.quiz ); 
     var errors = quiz.validate();//ya qe el objeto errors no tiene then(
      if (errors)
     {
-        var i=0; var errores=new Array();//se convierte en [] con la propiedad message por compatibilida con layout
+        var i=0; var errores=new Array();	//se convierte en [] con la propiedad message por compatibilida con layout
         for (var prop in errors) errores[i++]={message: errors[prop]};       
         res.render('quizes/new', {quiz: quiz, errors: errores});
     } else {
@@ -60,17 +59,17 @@
 
 
   // GET /quizes/:id/edit
-  exports.edit = function(req, res) {	console.log("Estoy en EDIT");
+  exports.edit = function(req, res) {			//console.log("Estoy en EDIT");
     var quiz = req.quiz;  // req.quiz: autoload de instancia de quiz
 	res.render('quizes/edit', {quiz: quiz, errors: []});
 };
 
   // PUT /quizes/:id
-  exports.update = function(req, res) {	console.log("Estoy en UPDATE");
+  exports.update = function(req, res) {			//console.log("Estoy en UPDATE");
     req.quiz.pregunta  = req.body.quiz.pregunta;
 	req.quiz.respuesta = req.body.quiz.respuesta;
     // req.quiz 
-	var errors = req.quiz.validate();//ya qe el objeto errors no tiene then(
+	var errors = req.quiz.validate();	//ya qe el objeto errors no tiene then(
     
      if (errors)
     {
@@ -96,7 +95,7 @@
 //  })
       
 		// DELETE /quizes/:id
-  exports.destroy = function(req, res) {  console.log("Estoy en DELETE");
+  exports.destroy = function(req, res) {  	//console.log("Estoy en DELETE");
     req.quiz.destroy().then( function() {
       res.redirect('/quizes');
     }).catch(function(error){next(error)});
